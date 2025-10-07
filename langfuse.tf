198208a078dd5463dbb0b982595ec09654309d24
mbeiro13 <martin.beiro@marvik.ai>
2025-10-07 12:45:37 -0300

    point to correct ref

--- langfuse.tf
+++ langfuse.tf
@@ -195,7 +195,7 @@ resource "kubernetes_secret" "langfuse" {
 
 resource "helm_release" "langfuse" {
   name       = "langfuse"
-  repository = "https://langfuse.github.io/langfuse-k8s"
+  repository = "git::https://github.com/levonlloyd/langfuse-k8s.git//charts/langfuse"
   version    = var.langfuse_chart_version
   chart      = "langfuse"
   namespace  = kubernetes_namespace.langfuse.metadata[0].name
